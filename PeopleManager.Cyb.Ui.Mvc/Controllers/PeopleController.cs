436511afe3bc97b18515fb9f813d66dcdc3a14c9
thibaultstadsbader <115148525+thibaultstadsbader@users.noreply.github.com>
2023-10-02 11:58:23 +0200

    Added partial view

---
+++ PeopleManager.Cyb.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Cyb.Ui.Mvc.Models;
+
+namespace PeopleManager.Cyb.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    private IList<Person> GetPeople()
+    {
+        return new List<Person>
+            {
+                new Person {FirstName = "Bavo", LastName = "Ketels", Email = "bavo.ketels@gmail.com"},
+                new Person {FirstName = "Bavo2", LastName = "Ketels2", Email = "bavo.ketels2@gmail.com"},
+                new Person {FirstName = "Bavo3", LastName = "Ketels3", Email = "bavo.ketels3@gmail.com"},
+                new Person {FirstName = "Bavo4", LastName = "Ketels4", Email = "bavo.ketels4@gmail.com"},
+            };
+    }
+}
